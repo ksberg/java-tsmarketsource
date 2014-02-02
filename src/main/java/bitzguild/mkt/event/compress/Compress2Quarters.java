00c80e8573e86913fecbdd14f46101b72e72d373
Kevin Berg <ksberg@gmail.com>
2014-02-02 11:44:42 -0800

    License Block

--- src/main/java/bitzguild/mkt/event/compress/Compress2Quarters.java
+++ src/main/java/bitzguild/mkt/event/compress/Compress2Quarters.java
@@ -1,11 +1,12 @@
 /* ***** BEGIN LICENSE BLOCK *****
- * 
- * Copyright (c) 2001-2013, Kevin Sven Berg
- * All rights reserved.
- * 
+ *
+ * Copyright (c) 2001-2014, Kevin Sven Berg. All rights reserved.
+ *
+ * This package is part of the Bitzguild Time Series Distribution
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  *     * Redistributions of source code must retain the above copyright
  *       notice, this list of conditions and the following disclaimer.
  *     * Redistributions in binary form must reproduce the above copyright
@@ -14,7 +15,7 @@
  *     * Neither the name of the <organization> nor the
  *       names of its contributors may be used to endorse or promote products
  *       derived from this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -25,7 +26,7 @@
  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  * ***** END LICENSE BLOCK ***** */
 
 package bitzguild.mkt.event.compress;
